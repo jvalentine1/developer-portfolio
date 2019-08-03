8ce945ca34be4ba8361ae06c08e8b8b16044f391
Jake <jake@Marissas-MacBook-Pro-2.local>
2019-08-02 21:25:30 -0600

    updated projects section

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,7 +1,76 @@
 //Modal Functions
 
-//project 1
+// ========== project 1 ==========
 $("#item-1").on("click", function() {
+
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    var modalDiv = $("<div>");
+    var projectTitle = $("<h3>");
+    projectTitle.text("React Click Game");
+    projectTitle.addClass("title-decorate");
+    modalDiv.append(projectTitle);
+
+     var br1 = $("<br>");
+     modalDiv.append(br1);
+
+     var aboutProject = $("<p>");
+     aboutProject.addClass("project-description");
+     aboutProject.text("This point and click game is built in React, and utilizes stateful and stateless components to make an enjoyable memory challenge for the user. Click the links below to see the deployed version and view the README and code!");
+     modalDiv.append(aboutProject);
+
+     var br2 = $("<br>");
+     modalDiv.append(br2);
+
+    var linkTitle = $("<h4>");
+    linkTitle.text("Heroku:");
+    modalDiv.append(linkTitle);
+
+    var br3 = $("<br>");
+    modalDiv.append(br3);
+
+    var link = $("<a>");
+    link.attr("target", "_blank");
+    link.attr("href", "https://jvalentine1.github.io/react-game/");
+    link.html("https://jvalentine1.github.io/react-game/");
+    modalDiv.append(link);
+
+    var br4 = $("<br>");
+    modalDiv.append(br4);
+
+    var linkTitle2 = $("<h4>");
+    linkTitle2.text("Github:");
+    linkTitle2.addClass("link-pad");
+    modalDiv.append(linkTitle2);
+
+    var br5 = $("<br>");
+    modalDiv.append(br5);
+
+    var link2 = $("<a>");
+    link2.attr("target", "_blank");
+    link2.attr("href", "https://github.com/jvalentine1/react-game");
+    link2.html("https://github.com/jvalentine1/react-game");
+    modalDiv.append(link2);
+
+     $(".modal-text").html(modalDiv);
+
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+      
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+});
+
+// ========== project 2 ==========
+$("#item-2").on("click", function() {
 
     var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
@@ -53,6 +122,75 @@ $("#item-1").on("click", function() {
     link2.attr("target", "_blank");
     link2.attr("href", "https://github.com/jvalentine1/friend-finder");
     link2.html("https://github.com/jvalentine1/friend-finder");
+    modalDiv.append(link2);
+
+     $(".modal-text").html(modalDiv);
+
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+      
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+});
+
+// =========== project 3 ===========
+$("#item-3").on("click", function() {
+
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    var modalDiv = $("<div>");
+    var projectTitle = $("<h3>");
+    projectTitle.text("News Scrape App");
+    projectTitle.addClass("title-decorate");
+    modalDiv.append(projectTitle);
+
+     var br1 = $("<br>");
+     modalDiv.append(br1);
+
+     var aboutProject = $("<p>");
+     aboutProject.addClass("project-description");
+     aboutProject.text("News Scrape is an application that utilizes Cheerio, Mongo, and Bootstrap to scrape articles from USA Today, and then, store and display them for the user. It also allows users to comment on articles and delete them from their network.  Click on the github links below to view the Read Me, code. Click the Heroku link to see the depoloyed version.");
+     modalDiv.append(aboutProject);
+
+     var br2 = $("<br>");
+     modalDiv.append(br2);
+
+    var linkTitle = $("<h4>");
+    linkTitle.text("Heroku:");
+    modalDiv.append(linkTitle);
+
+    var br3 = $("<br>");
+    modalDiv.append(br3);
+
+    var link = $("<a>");
+    link.attr("target", "_blank");
+    link.attr("href", "https://news-scrape-jv.herokuapp.com/");
+    link.html("https://news-scrape-jv.herokuapp.com/");
+    modalDiv.append(link);
+
+    var br4 = $("<br>");
+    modalDiv.append(br4);
+
+    var linkTitle2 = $("<h4>");
+    linkTitle2.text("Github:");
+    linkTitle2.addClass("link-pad");
+    modalDiv.append(linkTitle2);
+
+    var br5 = $("<br>");
+    modalDiv.append(br5);
+
+    var link2 = $("<a>");
+    link2.attr("target", "_blank");
+    link2.attr("href", "https://github.com/jvalentine1/news-scrape");
+    link2.html("https://github.com/jvalentine1/news-scrape");
     modalDiv.append(link2);
 
      $(".modal-text").html(modalDiv);
@@ -132,7 +270,7 @@ $("#item-5").on("click", function() {
     var modalDiv = $("<div>");
     var projectTitle = $("<h3>");
     projectTitle.addClass("title-decorate");
-    projectTitle.text("Mood Quotes App");
+    projectTitle.text("Giftastic");
     modalDiv.append(projectTitle);
 
      var br1 = $("<br>");
@@ -140,7 +278,7 @@ $("#item-5").on("click", function() {
 
      var aboutProject = $("<p>");
      aboutProject.addClass("project-description");
-     aboutProject.text("The Mood Quotes App utilizes four API's to provide the user with a fun and useful service. Upon email entry, the client will enter their location and be provided with the weather and an inspirational quote, they can also get a fact about the day and try some trivia. Click the github links below to view the Read Me, code, and deployed version!");
+     aboutProject.text("Giftastic is a front-end app that uses the giphy api to generate gif's for the user at the click of a button. It also allows te user to search for a new movie and append it to the list that is populated on load. Click the links below to see the deployed version and view the README and code.");
      modalDiv.append(aboutProject);
 
      var br2 = $("<br>");
@@ -155,8 +293,8 @@ $("#item-5").on("click", function() {
 
     var link = $("<a>");
     link.attr("target", "_blank");
-    link.attr("href", "https://jvalentine1.github.io/project-1-repo/");
-    link.html("https://jvalentine1.github.io/project-1-repo/");
+    link.attr("href", "https://jvalentine1.github.io/GifTastic/");
+    link.html("https://jvalentine1.github.io/GifTastic/");
     modalDiv.append(link);
 
     
@@ -171,8 +309,8 @@ $("#item-5").on("click", function() {
 
     var link2 = $("<a>");
     link2.attr("target", "_blank");
-    link2.attr("href", "https://github.com/jvalentine1/project-1-repo");
-    link2.html("https://github.com/jvalentine1/project-1-repo");
+    link2.attr("href", "https://github.com/jvalentine1/GifTastic");
+    link2.html("https://github.com/jvalentine1/GifTastic");
     modalDiv.append(link2);
 
      $(".modal-text").html(modalDiv);
